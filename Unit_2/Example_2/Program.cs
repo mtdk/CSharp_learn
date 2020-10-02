c48109d7ba284e0cc0f36076c4d92ef110df5654
mtdk <ruixiangxie@outlook.com>
2020-10-02 16:25:45 +0800

    202010021625

---
+++ Unit_2/Example_2/Program.cs
@@ -0,0 +1,12 @@
+﻿//引入空间名称
+using System;
+namespace Example_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Happy!");
+        }
+    }
+}
