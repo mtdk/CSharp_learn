ac53b67b8261e67839f0f7301f8517598f4f4f20
mtdk <ruixiangxie@outlook.com>
2020-10-02 16:13:32 +0800

    202010021613

---
+++ Unit_2/Example_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//两种空间引用模式
+//这是第一种
+using MyNamespace;
+
+namespace Example_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //这是第二种
+            //MyNamespace.MyClass obj = new MyNamespace.MyClass();
+            MyClass obj = new MyClass();
+        }
+    }
+}
+
+namespace MyNamespace
+{
+    public class MyClass { }
+}
