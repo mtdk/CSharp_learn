bdce9babaf3a7cf4bc673e833bfcea21b5822e35
mtdk <ruixiangxie@outlook.com>
2020-10-02 16:40:53 +0800

    202010021640

---
+++ Unit_2/Example_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_3
+{
+    class Test1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("入口点A");
+            Console.Read();
+        }
+    }
+
+    class Test2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("入口点B");
+            Console.Read();
+        }
+    }
+}
