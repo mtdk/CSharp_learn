c59b1869ae99fa8944297969aa6cc32abfc2827a
QINGHE-XRX <ruixiangxie@hotmail.com>
2021-06-05 18:08:04 +0800

    20210605007

---
+++ mysqlhelp_test/mysqlhelp_test/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using mysqlhelp_test;
+
+namespace mysqlhelp_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string loginID = textBox1.Text.Trim();
+            string loginPwd = textBox2.Text.Trim();
+            string sqlstr = "SELECT count(*) FROM `user` WHERE `user`.id = '' AND `user`.userPwd = ''";
+        }
+    }
+}
---
+++ mysqlhelp_test/mysqlhelp_test/mySqlHelp_test.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace mysqlhelp_test
+{
+    public static class mySqlHelp_test
+    {
+        private static readonly string connStr = "server=127.0.0.1;uid=testqh;pwd=123456;database=mtsystemdb;charset=utf8;";
+
+        //执行增、删、改的方法:ExcuteNonQuery()
+        public static int ExcuteNonQuery(string sql, params MySqlParameter[] pms)
+        {
+            using (MySqlConnection con = new MySqlConnection(connStr))
+            {
+                using(MySqlCommand cmd=new MySqlCommand(sql,con))
+                {
+                    if (pms != null)
+                    {
+                        cmd.Parameters.AddRange(pms);
+                    }
+                    con.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        //封装一个执行返回单个对象的方法:ExecuteScalar()
+        public static object ExecuteScalar(string sql, params MySqlParameter[] pms)
+        {
+            using (MySqlConnection con = new MySqlConnection(connStr))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(sql, con))
+                {
+                    if (pms != null)
+                    {
+                        cmd.Parameters.AddRange(pms);
+                    }
+                    con.Open();
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        //执行查询多行多列数据的方法:ExecuteReader
+        public static MySqlDataReader ExecuteReader(string sql, params MySqlParameter[] pms)
+        {
+            using (MySqlConnection con = new MySqlConnection(connStr))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(sql, con))
+                {
+                    if (pms != null)
+                    {
+                        cmd.Parameters.AddRange(pms);
+                    }
+                    try
+                    {
+                        con.Open();
+                        return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                    }
+                    catch (Exception)
+                    {
+                        con.Close();
+                        con.Dispose();
+                        throw;
+                    }
+                }
+            }
+        }
+
+        //执行返回DataTable的方法
+        public static DataTable ExcuteDataTable(string sql, params MySqlParameter[] pms)
+        {
+            DataTable dt = new DataTable();
+            using (MySqlDataAdapter adapter = new MySqlDataAdapter(sql, connStr))
+            {
+                if (pms != null)
+                {
+                    adapter.SelectCommand.Parameters.AddRange(pms);
+                }
+                adapter.Fill(dt);
+            }
+            return dt;
+        }
+    }
+}
